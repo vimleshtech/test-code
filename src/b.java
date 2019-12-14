e2a30cbd8876690c55e5fdfa4e20a05bd113027c
Kumar <vkumar15@ms.ds.uhc.com>
2019-12-14 15:24:16 +0530

    2.0

--- src/b.java
+++ src/b.java
@@ -4,7 +4,12 @@ public class b {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("b");
+		
+		int a=11;
+		int b=11;
+		
+		int c =a+b;
+		System.out.println(c);
 	}
 
 }
